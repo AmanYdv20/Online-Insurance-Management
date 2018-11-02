4999d670c1496f4d775624ab991399f547c04c7c
YdvAman20 <b160136@nitsikkim.ac.in>
2018-11-02 11:37:17 +0530

    vehicle part

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const express=require('express');
 const app=express();
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/insurance_system');
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-    res.render('index');
+var vehicleSchema=new mongoose.Schema({
+    name: String,
+    type: String,
+    description: String,
+    duration: Number,
+    created: {type: Date, default: Date.now}
 });
+
+var Vehicle=mongoose.model("Vehicle", vehicleSchema);
+
+//index routes
+app.get('/', function(req,res){
+    res.redirect("/vehicles");
+});
+
+//vehicle route
+app.get("/vehicles", function(req,res){
+    Vehicle.find({},function(err, vehicles){
+        if(err){
+            console.log("ERROR");
+        } else {
+            res.render("vehilces", {vehicles: vehicles});
+        }
+    })
+});
+
+// app.get("/vehicles/new", function(req, res){
+//     res.render("new");
+// });
 
 
 
